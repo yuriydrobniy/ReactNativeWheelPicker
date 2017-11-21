a6fd5cbe15034435b7cd749817035ed822a10728
Renan Lazarini Gil <renan.lzgil@gmail.com>
2017-11-21 13:14:06 +0100

    adding lint

--- TimePicker.js
+++ TimePicker.js
@@ -1,11 +1,8 @@
 import React from 'react';
-import {I18nManager} from 'react-native';
+import { I18nManager } from 'react-native';
 import PropTypes from 'prop-types';
 
-import {
-  View,
-  StyleSheet,
-} from 'react-native';
+import { View, StyleSheet } from 'react-native';
 import WheelPicker from './WheelPicker';
 import moment from 'moment';
 
@@ -18,22 +15,25 @@ const styles = StyleSheet.create({
   wheelPicker: {
     height: 150,
     width: null,
-    flex: 1,
-  },
+    flex: 1
+  }
 });
 
 class TimePicker extends React.Component {
   constructor(props) {
     super(props);
-    this.selectedDate = this.props.initDate ? new Date(this.props.initDate) : new Date();
+    this.selectedDate = this.props.initDate
+      ? new Date(this.props.initDate)
+      : new Date();
 
     this.hours = this.props.hours ? this.props.hours : getHoursArray();
-    this.minutes = this.props.minutes ? this.props.minutes : getFiveMinutesArray();
+    this.minutes = this.props.minutes
+      ? this.props.minutes
+      : getFiveMinutesArray();
 
-    const minutesCount = this.props.minutes ? this.props.minutes.length : 12
-    const minutesInHour = 60
+    const minutesCount = this.props.minutes ? this.props.minutes.length : 12;
+    const minutesInHour = 60;
     this.initMinuteInex = Math.round(this.selectedDate.getMinutes());
-
 
     if (!this.props.is24hours) {
       const time12format = hourTo12Format(this.selectedDate.getHours());
@@ -42,7 +42,6 @@ class TimePicker extends React.Component {
     } else {
       this.initHourInex = this.selectedDate.getHours() - 1;
     }
-
   }
 
   render() {
@@ -70,16 +69,18 @@ class TimePicker extends React.Component {
           onItemSelected={data => this.onMinuteSelected(data)}
           selectedItemPosition={this.initMinuteInex}
         />
-        <WheelPicker
-          style={styles.wheelPicker}
-          isAtmospheric
-          isCurved
-          visibleItemCount={6}
-          data={getAmArray()}
-          selectedItemTextColor={'black'}
-          onItemSelected={data => this.onAmSelected(data)}
-          selectedItemPosition={this.initAmInex}
-        />
+        {!this.props.is24hours && (
+          <WheelPicker
+            style={styles.wheelPicker}
+            isAtmospheric
+            isCurved
+            visibleItemCount={6}
+            data={getAmArray()}
+            selectedItemTextColor={'black'}
+            onItemSelected={data => this.onAmSelected(data)}
+            selectedItemPosition={this.initAmInex}
+          />
+        )}
       </View>
     );
   }
@@ -110,14 +111,13 @@ class TimePicker extends React.Component {
       this.props.onTimeSelected(this.selectedDate);
     }
   }
-
 }
 
 TimePicker.propTypes = {
   initDate: PropTypes.string,
   onTimeSelected: PropTypes.func,
   hours: PropTypes.array,
-  minutes: PropTypes.array,
+  minutes: PropTypes.array
 };
 
 // it takes in format '12 AM' and return 24 format
@@ -132,18 +132,19 @@ function hourTo12Format(hour) {
   return dateTo12Hour(currDate.toISOString());
 }
 
-const dateTo12Hour = (dateString) => {
+const dateTo12Hour = dateString => {
   const localDate = new Date(dateString);
   let hour = localDate.getHours();
   if (hour === 12) {
-    return [('12'), ('PM')];
-  } if (hour === 0) {
-    return [('12'), ('AM')];
+    return ['12', 'PM'];
+  }
+  if (hour === 0) {
+    return ['12', 'AM'];
   }
   const afterMidday = hour % 12 === hour;
   hour = afterMidday ? hour : hour % 12;
   const amPm = afterMidday ? 'AM' : 'PM';
-  return [(hour.toString()), (amPm)];
+  return [hour.toString(), amPm];
 };
 
 function getHoursArray() {
